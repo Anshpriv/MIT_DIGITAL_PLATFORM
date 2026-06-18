ab4549a7647a0daf84d65639ee2ab039d90a0ba6
Ansh <anshthakare542@gmail.com>
2026-06-18 12:31:21 +0530

    chore: Externalize firebase-admin in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.45.69", "10.252.186.69", "localhost"],
+  serverExternalPackages: ["firebase-admin"],
 };
 
 export default nextConfig;
